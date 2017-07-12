7bdbcc2bc9089d44ab147d82639c41244afa1a0b
Wei-ju Wu <weiju.wu@gmail.com>
2017-07-12 10:04:43 -0700

    refactoring: moved source around
    separate locations for R source, NEO and Python code

--- example_cMonkey2_files/start_cmonkey.sh
+++ example_cMonkey2_files/start_cmonkey.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../cmonkey2/bin/cmonkey2.sh --organism hsa --pipeline ../../pipeline.json --config ../../config_pita.ini --rsat_organism Homo_sapiens --rsat_dir ../../commonFiles/data/Homo_sapiens --string ../../commonFiles/geneMania.tsv --synonym_file ../../commonFiles/synonymsThesaurus.csv --verbose --case_sensitive --numclusters 534 ../../exprs/mesothelioma_norm_cv_z.tsv
+cmonkey2 --organism hsa --pipeline ../../pipeline.json --config ../../config_pita.ini --rsat_organism Homo_sapiens --rsat_dir ../../commonFiles/data/Homo_sapiens --string ../../commonFiles/geneMania.tsv --synonym_file ../../commonFiles/synonymsThesaurus.csv --verbose --case_sensitive --numclusters 534 ../../exprs/mesothelioma_norm_cv_z.tsv
